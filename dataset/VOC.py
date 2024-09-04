52cce4839dd1e66482f8ea0152b409557c4c1a04
QLYYLQ <1965658150@qq.com>
2024-09-04 12:48:24 +0800

    first version about dataset

--- dataset/VOC.py
+++ dataset/VOC.py
@@ -9,8 +9,9 @@ import torch
 import torchvision as tv
 from PIL import Image
 from torch import distributed
-from .utils.filter_images import filter_images
-from .register import register_training_dataset,register_evaluating_dataset
+from .utils.filter_images import filter_images, save_list_from_filter, load_list_from_path
+from .register import register_training_dataset, register_evaluating_dataset
+
 
 # classes = {
 #     0: 'background',
@@ -40,15 +41,16 @@ from .register import register_training_dataset,register_evaluating_dataset
 @register_training_dataset
 class Segmentation(data.Dataset):
     """
+    remove:
+        image_set (string, optional): Select the image_set to use, ``train``, ``trainval`` or ``val``
     Args:
         root (string): Root directory of the VOC Dataset.
-        image_set (string, optional): Select the image_set to use, ``train``, ``trainval`` or ``val``
         is_aug (bool, optional): If you want to use the augmented train set or not (default is True)
         transform (callable, optional): A function/transform that  takes in an PIL image
             and returns a transformed version. E.g, ``transforms.RandomCrop``
     """
 
-    def __init__(self, root, is_aug=True,transform=None):
+    def __init__(self, root, is_aug=True, transform=None, target_transform=None):
         """
         删除：image_set='train'
         因为测试使用别的数据集（调控是否只返回特定的图片）
@@ -59,11 +61,12 @@ class Segmentation(data.Dataset):
         transform
         """
         self.root = os.path.expanduser(root)
-        self._check_dataset_exists()
+        self._check_dataset_exists(self.root)
         self.transform = transform
+        self.target_transform = target_transform
         # self.image_set = image_set
         splits_dir = os.path.join(self.root, 'splits')
-        if is_aug: # and image_set == 'train':
+        if is_aug:  # and image_set == 'train':
             mask_dir = os.path.join(self.root, 'SegmentationClassAug')
             assert os.path.exists(mask_dir), "SegmentationClassAug not found"
             split_f = os.path.join(splits_dir, 'train_aug.txt')
@@ -79,9 +82,9 @@ class Segmentation(data.Dataset):
 
         self.images = self.load_data_path_to_list(split_f)
 
-    def _check_dataset_exists(self):
+    def _check_dataset_exists(self, path):
         # 检测是否存在数据集
-        if not os.path.isdir(self.root):
+        if not os.path.isdir(path):
             raise RuntimeError(
                 'Dataset not found or corrupted.'
             )
@@ -92,7 +95,7 @@ class Segmentation(data.Dataset):
             for line in f:
                 x = line[:-1].split(' ')
                 images.append((
-                    os.path.join(self.root, "VOCdevkit/VOC2012", x[0][1:]),
+                    os.path.join(self.root, x[0][1:]),
                     os.path.join(self.root, x[1][1:])
                 ))
 
@@ -109,17 +112,20 @@ class Segmentation(data.Dataset):
         img = Image.open(self.images[index][0]).convert('RGB')
         if self.transform is not None:
             img, target = self.transform(img, target)
-        return {"data":[img, target],"path":[self.images[index][0],self.images[index][1]]}
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return {"data": [img, target], "path": [self.images[index][0], self.images[index][1]]}
 
-    def viz_getter(self, index):
-        image_path = self.images[index][0]
-        raw_image = Image.open(self.images[index][0]).convert('RGB')
-        target = Image.open(self.images[index][1])
-        if self.transform is not None:
-            img, target = self.transform(raw_image, target)
-        else:
-            img = copy.deepcopy(raw_image)
-        return image_path, raw_image, img, target
+    def apply_new_data_list(self, new_data_list_path):
+        """真个函数是用作训练中不同的task构建不同内容的Segmentation子集时使用"""
+        self._check_dataset_exists(new_data_list_path)
+        self.images = []
+        # 这个数据集用空格做分割，但是考虑到很多时候路径带空格，这套方法会有问题
+        # self.images = self.load_data_path_to_list(new_data_list_path)
+        with open(new_data_list_path, "r") as f:
+            for line in f:
+                x = line[:-1].split(',')
+                self.images.append((x[0], x[1]))
 
     def __len__(self):
         return len(self.images)
@@ -133,17 +139,18 @@ class Increment(data.Dataset):
             root,
             train=True,
             transform=None,
-            order = None,
+            order=None,
             labels=None,
             labels_old=None,
             idxs_path=None,
+            save_path = None,
             masking=True,
             overlap=True,
             data_masking="current",
             **kwargs
     ):
 
-        full_voc = Segmentation(root, 'train' if train else 'val', is_aug=True, transform=None)
+        full_voc = Segmentation(root, is_aug=True, transform=None)
         self.order = order
         self.labels = []
         self.labels_old = []
@@ -162,7 +169,6 @@ class Increment(data.Dataset):
             # 下面这一坨注释是：确保了在学习新任务时，模型能够保留足够的旧任务示例，从而在持续学习过程中保持对旧任务的记忆。它通过平衡每个类别的
             # 示例数量来防止灾难性遗忘，同时也考虑了数据集之间可能存在的重叠，如果用sam的话我们应该不需要这个流程
 
-
             # if ssul_exemplar_path is not None:
             #     print(ssul_exemplar_path)
             #     if os.path.exists(ssul_exemplar_path):
@@ -180,42 +186,24 @@ class Increment(data.Dataset):
             #         assert idxs_path is not None
             #         idxs_old = np.load(idxs_path).tolist()
             #
-            #         old_exemplar = (f'./balance_step_exemplar/{opts.dataset}_{opts.task}_step_{opts.step - 1}_exemplar'
-            #                         f'.pth')
-            #         ssul_exemplar_idx_cls = torch.load(old_exemplar)
-            #         lens = {}
-            #         for label in labels_old:
-            #             ssul_exemplar_idx_cls[label - 1] = []
-            #             lens[label - 1] = 0
-            #         for idx in tqdm(idxs_old):
-            #             img_cls = np.unique(np.array(full_voc[idx][1]))
-            #             fg = 1
-            #             print(lens)
-            #             for label in img_cls:  #QUESTION??? NOT EVERY CLASS TO BE 20 SAMPLES
-            #                 if label in ssul_exemplar_idx_cls.keys() and lens[label] < per_task_exemplar:
-            #                     ssul_exemplar_idx_cls[label].append(idx)
-            #                     idxs.append(idx)
-            #                     lens[label] += 1
-            #             for label in labels_old:
-            #                 if lens[label - 1] < per_task_exemplar:
-            #                     fg = 0
-            #             if fg == 1:
-            #                 break
+            # old_exemplar = (f'./balance_step_exemplar/{opts.dataset}_{opts.task}_step_{opts.step - 1}_exemplar'
+            # f'.pth') ssul_exemplar_idx_cls = torch.load(old_exemplar) lens = {} for label in labels_old:
+            # ssul_exemplar_idx_cls[label - 1] = [] lens[label - 1] = 0 for idx in tqdm(idxs_old): img_cls =
+            # np.unique(np.array(full_voc[idx][1])) fg = 1 print(lens) for label in img_cls:  #QUESTION??? NOT EVERY
+            # CLASS TO BE 20 SAMPLES if label in ssul_exemplar_idx_cls.keys() and lens[label] < per_task_exemplar:
+            # ssul_exemplar_idx_cls[label].append(idx) idxs.append(idx) lens[label] += 1 for label in labels_old: if
+            # lens[label - 1] < per_task_exemplar: fg = 0 if fg == 1: break
             #
             #         torch.save(ssul_exemplar_idx_cls, ssul_exemplar_path)
 
-
             # take index of images with at least one class in labels and all classes in labels+labels_old+[0]
 
-
             if idxs_path is not None and os.path.exists(idxs_path):
-                idx = np.load(idxs_path).tolist()
+                idx = load_list_from_path(idxs_path)
             else:
                 idx = filter_images(full_voc, labels, labels_old, overlap=overlap)
-                if idxs_path is not None and distributed.get_rank() == 0:
-                    np.save(idxs_path, np.array(idx, dtype=int))
-
-
+                if save_path is not None:
+                    save_list_from_filter(idx, save_path)
 
             #if train:
             #    masking_value = 0
@@ -224,8 +212,8 @@ class Increment(data.Dataset):
 
             #self.inverted_order = {label: self.order.index(label) for label in self.order}
             #self.inverted_order[255] = masking_value
-
-            masking_value = 0  # Future classes will be considered as background.
+            # 只在训练
+            masking_value = 255  # Future classes will be considered as background.
             self.inverted_order = {label: self.order.index(label) for label in self.order}
             self.inverted_order[255] = 255
 
@@ -253,16 +241,15 @@ class Increment(data.Dataset):
                     apply_(lambda x: self.inverted_order[x] if x in tmp_labels else masking_value)
                 )
             else:
-                assert False
                 target_transform = reorder_transform
+                assert False
 
             # make the subset of the dataset
-            self.dataset = Subset(full_voc, idxs, transform, target_transform)
+            self.dataset = full_voc
+            self.dataset.images = idx
+            self.dataset.target_transform = target_transform
         else:
             self.dataset = full_voc
-
-
-
 
     def __getitem__(self, index):
         """
@@ -273,9 +260,6 @@ class Increment(data.Dataset):
         """
 
         return self.dataset[index]
-
-    def viz_getter(self, index):
-        return self.dataset.viz_getter(index)
 
     def __len__(self):
         return len(self.dataset)
--- dataset/utils/filter_images.py
+++ dataset/utils/filter_images.py
@@ -1,9 +1,10 @@
 import numpy as np
-
+import os.path as osp
 
 def filter_images(dataset, labels, labels_old, overlap):
     """
     Filter images according to the overlap.
+    create a list of images and mask paths which is use for filtering.
     Also used to create the index_list.txt from tasks in config
 
     Parameters
@@ -29,8 +30,19 @@ def filter_images(dataset, labels, labels_old, overlap):
     for i in range(len(dataset)):
         cls = np.unique(np.array(dataset[i]["data"][1]))
         if fil(cls):
-            index.append(i)
+            index.append((dataset[i]["path"][0],dataset[i]["path"][1]))
         if i % 1000 == 0:
             print(f"\t{i}/{len(dataset)} ...")
     return index
 
+def save_list_from_filter(index,save_path):
+    with open(save_path,"w") as f:
+        for pair in index:
+            f.write(f"{pair[0]},{pair[1]}\n")
+
+def load_list_from_path(index,save_path):
+    new_list = []
+    with open(save_path,"r") as f:
+        for line in f:
+            x = line[:-1].split(",")
+            new_list.append((x[0],x[1]))
