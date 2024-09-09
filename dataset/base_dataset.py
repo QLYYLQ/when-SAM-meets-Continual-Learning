1848a7c766eb17495350392aa52cab97b9133634
QLYYLQ <1965658150@qq.com>
2024-09-10 04:13:46 +0800

    increment dataset version 2

--- dataset/base_dataset.py
+++ dataset/base_dataset.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional,List,Callable
+from typing import Optional, List, Callable
 import numpy as np
 import torchvision as tv
 from torch.utils.data import Dataset
@@ -18,7 +18,8 @@ class BaseSegmentation(Dataset):
                  target_transform: Optional[Callable] = None,
                  need_index_name: bool = True,
                  classes: Optional[dict] = None,
-                 ignore_index: Optional[List] = None):
+                 ignore_index: Optional[List] = None,
+                 mask_value: Optional[int] = None):
 
         if ignore_index is None:
             # 一般target中255都是忽略的地方（黑色背景）
@@ -26,12 +27,18 @@ class BaseSegmentation(Dataset):
         else:
             self.ignore_index = ignore_index
         self.root = root
+        self.mask_value = mask_value
         self._check_path_exists(root)
-        if transform == "default":
-            self.transform = tv.transforms.ToTensor()
+
+        if not transform:
+            self._init_image_transform()
         else:
             self.transform = transform
-        self.target_transform = target_transform
+        if not target_transform:
+            self._init_target_transform()
+        else:
+            self.target_transform = target_transform
+
         self.train = train
         self.need_index_name = need_index_name
         if need_index_name and classes is None:
@@ -50,6 +57,18 @@ class BaseSegmentation(Dataset):
                 f'path not found or corrupted and the path is {path}'
             )
 
+    def _init_target_transform(self):
+        """把一些需要忽略掉的label对应的target换成255"""
+        target_transform = tv.transforms.Lambda(
+            lambda t: t.apply_(
+                lambda x: x if x not in self.ignore_index else self.mask_value
+            )
+        )
+        self.target_transform = tv.transforms.Compose([tv.transforms.ToTensor(), target_transform])
+
+    def _init_image_transform(self):
+        self.image_transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+
     def _get_path(self):
         """这个类需要被重写，引导到储存文件图片路径的文档，默认是root_dir下list中train.txt"""
         return os.path.join(self.root, "list", 'train.txt')
@@ -60,7 +79,8 @@ class BaseSegmentation(Dataset):
         with open(path, 'r') as f:
             for line in f:
                 x = line.strip().split(",")
-                images.append((os.path.join(self.root, x[0]).replace(os.sep,"/"), os.path.join(self.root, x[1]).replace(os.sep,"/")))
+                images.append((os.path.join(self.root, x[0]).replace(os.sep, "/"),
+                               os.path.join(self.root, x[1]).replace(os.sep, "/")))
         return images
 
     def apply_new_data_list(self, new_data_list_path):
@@ -102,20 +122,105 @@ class BaseSegmentation(Dataset):
         return len(self.images)
 
 
-class BaseIncrement(Dataset):
+# class BaseIncrement(Dataset):
+#
+#     def __init__(self,
+#                  segmentation_dataset_name: str = None,
+#                  segmentation_config: dict = None,
+#                  train: bool = True,
+#                  labels: list = None,
+#                  labels_old: list = None,
+#                  overlap: bool = True,
+#                  masking: bool = True,
+#                  data_masking: str = "current",
+#                  no_memory: bool = True,
+#                  new_image_path: str = None,
+#                  save_stage_image_list_path: str = None,
+#                  mask_value: int = 255):
+#         self.no_memory = no_memory
+#         if not self.no_memory:
+#             raise NotImplementedError("not implemented")
+#
+#         self.dataset = dataset_entrypoints(segmentation_dataset_name)(**segmentation_config)
+#         self.ignore_index = self.dataset.ignore_index
+#         self.__strip_ignore(labels)
+#         self.__strip_ignore(labels_old)
+#         assert not any(i in labels_old for i in labels)  # 排除忽略的index以后，之前stage训练的label和当前stage训练的label要互斥
+#
+#         if new_image_path is not None and os.path.exists(new_image_path):
+#             idx = load_list_from_path(new_image_path)
+#         else:
+#             idx = filter_images(self.dataset, labels, labels_old, overlap=overlap)
+#             if save_stage_image_list_path is not None:
+#                 save_list_from_filter(idx, save_stage_image_list_path)
+#
+#         self.dataset.images = idx
+#
+#         self.train = train
+#
+#         self.order = self.dataset.get_class_index()
+#         self.labels = labels
+#         self.labels_old = labels_old
+#         self.data_masking = data_masking
+#         self.overlap = overlap
+#         self.masking = masking
+#         self.mask_value = mask_value
+#         self._create_inverted_order()
+#         self.dataset.target_transform = self._create_target_transform()
+#
+#     def __strip_ignore(self, labels):
+#         for i in self.ignore_index:
+#             while i in labels:
+#                 labels.remove(i)
+#
+#     def _create_inverted_order(self, mask_value=255):
+#         # 映射label和索引
+#         self.inverted_order = {label: self.order.index(label) for label in self.order if label not in self.ignore_index}
+#
+#     def _create_target_transform(self):
+#         mask_value = self.mask_value
+#         target_transform = tv.transforms.Lambda(
+#             lambda t: t.apply_(
+#                 lambda x: x if x in self.order else mask_value
+#             )
+#         )
+#
+#         if self.masking:
+#             if self.data_masking == "current":
+#                 tmp_labels = self.labels + [mask_value]
+#             elif self.data_masking == "current+old":
+#                 tmp_labels = self.labels_old + self.labels + [255]
+#             # elif self.data_masking == "all":
+#             #     # 全部保留
+#             #     target_transform = None
+#             # # elif self.data_masking == "new":
+#             # #     tmp_labels = self.labels
+#             # #     masking_value = 255
+#             #
+#             target_transform = tv.transforms.Lambda(
+#                 lambda t: t.
+#                 apply_(lambda x: x if x in tmp_labels else mask_value)
+#             )
+#         return target_transform
+#
+#     def __getitem__(self, index):
+#         return self.dataset[index]
+#
+#     def __len__(self):
+#         return len(self.dataset)
 
+
+class BaseIncrement(Dataset):
     def __init__(self,
                  segmentation_dataset_name: str = None,
                  segmentation_config: dict = None,
+                 stage_index_dict: dict = None,
+                 index_path_dict: dict = None,
                  train: bool = True,
-                 labels: list = None,
-                 labels_old: list = None,
                  overlap: bool = True,
                  masking: bool = True,
                  data_masking: str = "current",
                  no_memory: bool = True,
-                 new_image_path: str = None,
-                 save_stage_image_list_path: str = None,
                  mask_value: int = 255):
         self.no_memory = no_memory
         if not self.no_memory:
@@ -123,24 +228,20 @@ class BaseIncrement(Dataset):
 
         self.dataset = dataset_entrypoints(segmentation_dataset_name)(**segmentation_config)
         self.ignore_index = self.dataset.ignore_index
-        self.__strip_ignore(labels)
-        self.__strip_ignore(labels_old)
-        assert not any(i in labels_old for i in labels)  # 排除忽略的index以后，之前stage训练的label和当前stage训练的label要互斥
+        self.stage_index_dict = stage_index_dict
+        self.index_path_dict = index_path_dict
+        self.labels = []
+        self.labels_old = []
+        self.stage = 0
+        self.update_stage(0)
 
-        if new_image_path is not None and os.path.exists(new_image_path):
-            idx = load_list_from_path(new_image_path)
-        else:
-            idx = filter_images(self.dataset, labels, labels_old, overlap=overlap)
-            if save_stage_image_list_path is not None:
-                save_list_from_filter(idx, save_stage_image_list_path)
-        
-        self.dataset.images = idx
-        
+        self.__strip_ignore(self.labels)
+        self.__strip_ignore(self.labels_old)
+        assert not any(i in self.labels_old for i in self.labels)  # 排除忽略的index以后，之前stage训练的label和当前stage训练的label要互斥
+
         self.train = train
-        
+
         self.order = self.dataset.get_class_index()
-        self.labels = labels
-        self.labels_old = labels_old
         self.data_masking = data_masking
         self.overlap = overlap
         self.masking = masking
@@ -164,27 +265,34 @@ class BaseIncrement(Dataset):
                 lambda x: x if x in self.order else mask_value
             )
         )
-
         if self.masking:
             if self.data_masking == "current":
                 tmp_labels = self.labels + [mask_value]
             elif self.data_masking == "current+old":
                 tmp_labels = self.labels_old + self.labels + [255]
-            # elif self.data_masking == "all":
-            #     # 全部保留
-            #     target_transform = None
-            # # elif self.data_masking == "new":
-            # #     tmp_labels = self.labels
-            # #     masking_value = 255
-            #
             target_transform = tv.transforms.Lambda(
                 lambda t: t.
                 apply_(lambda x: x if x in tmp_labels else mask_value)
             )
-        return target_transform
+        total_transform = tv.transforms.Compose([tv.transforms.ToTensor(), target_transform])
+        return total_transform
 
     def __getitem__(self, index):
         return self.dataset[index]
 
     def __len__(self):
         return len(self.dataset)
+
+    def update_stage(self, stage_number):
+        max_stage = len(self.stage_index_dict.keys())
+        if stage_number >= max_stage:
+            raise ValueError("stage number out of range")
+        if stage_number == 0:
+            labels_old = []
+            labels = self.stage_index_dict[stage_number]
+        else:
+            labels = self.stage_index_dict[stage_number]
+            labels_old = self.stage_index_dict[stage_number - 1]
+        self.labels = labels
+        self.labels_old = labels_old
+        self.dataset.apply_new_data_list()
